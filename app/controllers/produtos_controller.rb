c4655a706ff1aa88d3130b5c7721cfd6b4008850
zabotg <zabot.gui@gmail.com>
2017-04-15 11:18:23 -0300

    Implementacao da busca

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -5,6 +5,11 @@ class ProdutosController < ApplicationController
       @produtos_por_preco = Produto.order(:preco).limit 2
    end
 
+   def busca
+      @nome_a_buscar = params[:nome]
+      @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+   end
+
    def create
       valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
       produto = Produto.create valores
