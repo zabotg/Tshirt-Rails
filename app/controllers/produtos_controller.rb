8f34a1f974f45081c50dca93c24b1436b41777cb
zabotg <zabot.gui@gmail.com>
2017-04-17 10:57:26 -0300

    Partials com locals

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,8 +1,15 @@
 class ProdutosController < ApplicationController
 
+   before_action :set_produto, only: [:edit, :update, :destroy]
+
    def index
       @produtos_por_nome = Produto.order(:nome).limit 5
       @produtos_por_preco = Produto.order(:preco).limit 2
+   end
+
+   def new
+      @produto = Produto.new
+      renderiza :new
    end
 
    def busca
@@ -10,27 +17,48 @@ class ProdutosController < ApplicationController
       @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
    end
 
+   def edit
+      renderiza :edit
+   end
+
+   def update
+      if @produto.update produto_params
+         flash[:notice] = "Produto atualizado com sucesso!"
+         redirect_to root_url
+      else
+         renderiza :edit
+      end
+   end
+
    def create
-      valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
-      @produto = Produto.new valores
+      @produto = Produto.new produto_params
       if @produto.save
          flash[:notice] = "Produto salvo com sucesso!"
          redirect_to root_url
       else
-         render :new
+         renderiza :new
       end
    end
 
    def destroy
-      id = params[:id]
-      Produto.destroy id
+      @produto.destroy
       redirect_to root_url
    end
 
-   def new
-    @produto = Produto.new
-    @departamentos = Departamento.all
-    render :new
-end
+private
+
+   def renderiza(view)
+      @departamentos = Departamento.all
+      render view
+   end
+
+   def set_produto
+      id = params[:id]
+      @produto = Produto.find(id)
+   end
+
+   def produto_params
+         params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
+   end
 
 end
